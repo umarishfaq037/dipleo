d6ad8808c38d087a324137f8121a3c8c45cfafc7
Umar Ishfaq <umar_ishfaq@hotmail.com>
2020-01-20 22:05:51 +0500

    updated user models

--- user/admin.py
+++ user/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Users
+from .models import Users,Profile
 
 admin.site.register(Users)
+admin.site.register(Profile)
 
 # Register your models here.
